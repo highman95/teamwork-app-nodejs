063a732fadb5ce148789e4a27070541f61011796
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-12 15:26:00 +0100

    refactor(pool): add console-reporting of DB ops

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,14 @@
-const pg = require('pg');
+const { Pool } = require('pg');
 
-const db = new pg.Pool({
+const db = new Pool({
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT,
 });
+
+db.on('connect', () => console.log('Connected to database...'));
+db.on('error', (e) => console.error(`Cannot connect to database...${e.message}`));
 
 module.exports = db;
