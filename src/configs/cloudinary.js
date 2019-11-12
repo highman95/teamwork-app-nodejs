9132a61588a65c4de4232fe209bf2807c5031901
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-12 19:28:23 +0100

    chore(bootstrap): add cloudinary configuration

---
+++ src/configs/cloudinary.js
@@ -0,0 +1,16 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+module.exports = {
+    uploadImage: async (file, callBack) => {
+        const options = { resource_type: 'image', folder: 'capstone-project/images/' };
+        const response = await cloudinary.uploader.upload(file, options, callBack);
+        return response;
+    },
+};
