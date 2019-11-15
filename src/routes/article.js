1919b5da2f009e95c1e29b4601ed7e1c754fbf59
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-15 05:09:33 +0100

    feature(article-update): add route/middleware to update  an article [Delivers #8]

--- src/routes/article.js
+++ src/routes/article.js
@@ -4,6 +4,7 @@ const auth = require('../middlewares/auth');
 module.exports = (router) => {
     router.post('/articles', auth, articleController.createOne);
     router.post('/articles/:articleId/comment', auth, articleController.createPostComment);
+    router.patch('/articles/:articleId', auth, articleController.updatePost);
     router.get('/articles/:articleId', auth, articleController.getPost);
     router.delete('/articles/:articleId', auth, articleController.deletePost);
 };
