b27bc52069432a26e1903d529764e3c436ef887c
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-15 07:48:15 +0100

    feature(feed): implement middleware for fetching feeds [Finishes #32]

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const articleRoutes = require('./article');
 const gifRoutes = require('./gif');
 const userRoutes = require('./user');
+const feedRoutes = require('./feed');
 
 module.exports = (router) => {
     articleRoutes(router);
     gifRoutes(router);
     userRoutes(router);
+    feedRoutes(router);
 
     return router;
 };
