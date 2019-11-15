b31f1fe145cc143ccb2997456d017789e8692c79
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-15 05:58:33 +0100

    chore(auth): add authentication to the create-user route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const userController = require('../controllers/user');
+const auth = require('../middlewares/auth');
 
 module.exports = (router) => {
-    router.post('/auth/create-user', userController.createOne);
+    router.post('/auth/create-user', auth, userController.createOne);
     router.post('/auth/signin', userController.signIn);
 };
