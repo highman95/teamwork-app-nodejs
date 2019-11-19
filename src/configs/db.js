af2ff891d7fb2a0a784c5ac612460c1c9e87b8c2
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-19 23:25:14 +0100

    chore(database): modify db connection config for heroku deployment

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const db = new Pool({
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.DB_SSL === 'true',
 });
 
 db.on('connect', () => console.log('Connected to database...'));
