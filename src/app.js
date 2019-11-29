f52a3b3b253a9d795b6b0fefb3a061e705dd1e6c
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2019-11-29 17:42:30 +0100

    chore(header): add token to the list of acceptable headers

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const router = express.Router();
 // handle CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, token');
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST, GET, PUT, DELETE, PATCH');
     next();
 });
