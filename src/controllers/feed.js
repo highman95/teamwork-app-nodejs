50cdc8ee0dd0b0e67b8620cb5a87ed64c8e840a7
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-01-15 12:34:15 +0100

    refactor(controller): update use of async/await

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -2,32 +2,29 @@ const db = require('../configs/db');
 
 module.exports = {
     getPosts: async (req, res) => {
-        db.query('SELECT p.id, title, content, image_url, user_id, post_type_id, created_at, name FROM posts p JOIN post_types pt ON pt.id = p.post_type_id ORDER BY created_at DESC', [], (err, result) => {
-            try {
-                if (err) {
-                    throw err;
-                }
+        try {
+            const result = await db.query('SELECT p.id, title, content, image_url, user_id, post_type_id, created_at, name FROM posts p JOIN post_types pt ON pt.id = p.post_type_id ORDER BY created_at DESC');
 
-                const feeds = result.rows.map((row) => {
-                    const {
-                        id, title, content: article, image_url: url, post_type_id: postTypeId,
-                        user_id: authorId, created_at: createdOn, name: type,
-                    } = row;
+            const feeds = result.rows.map((row) => {
+                const {
+                    id, title, content: article, image_url: url, post_type_id: postTypeId,
+                    user_id: authorId, created_at: createdOn, name: type,
+                } = row;
 
-                    return {
-                        id,
-                        createdOn,
-                        title,
-                        ...(postTypeId === 1 ? { url } : { article }),
-                        authorId, type,
-                    };
-                });
+                return {
+                    id,
+                    createdOn,
+                    title,
+                    ...(postTypeId === 1 ? { url } : { article }),
+                    authorId,
+                    type,
+                };
+            });
 
-                res.status(200).json({ status: 'success', data: feeds });
-            } catch (e) {
-                console.error('[Feed] DB-Error', e.message || e.error.message);
-                res.status(500).json({ status: 'error', error: 'The article/gif posts could not be retrieved' });
-            }
-        });
+            res.status(200).json({ status: 'success', data: feeds });
+        } catch (e) {
+            console.error('[Feed] DB-Error', e.message || e.error.message);
+            res.status(500).json({ status: 'error', error: 'The article/gif posts could not be retrieved' });
+        }
     },
 };
--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,108 +1,117 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const db = require('../configs/db');
-const util = require('util');
 
 const generateToken = (payload) => jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '24h', subject: 'TeamWork-nodeJS' });
 
 module.exports = {
-    createOne: (req, res) => {
+    createOne: async (req, res) => {
         const {
             firstName, lastName, email, password, gender, address, jobRole, department,
         } = req.body;
 
-        if (firstName === null || firstName === undefined || firstName.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: firstName' });
-        } else if (lastName === null || lastName === undefined || lastName.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: lastName' });
-        } else if (email === null || email === undefined || email.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: email address' });
-        } else if (password === null || password === undefined || password.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: password' });
-        } else if (gender === null || gender === undefined || gender.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: gender' });
-        } else if (jobRole === null || jobRole === undefined || jobRole.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: jobRole' });
-        } else if (department === null || department === undefined || department.trim() === '') {
-            res.status(400).json({ status: 'error', error: 'Missing required parameter: department' });
+        if (!firstName || firstName.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'FirstName is missing' });
+        } else if (!lastName || lastName.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'LastName is missing' });
+        } else if (!email || email.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'Email-address is missing' });
+        } else if (!password || password.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'Password is missing' });
+        } else if (!gender || gender.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'Gender is missing' });
+        } else if (!jobRole || jobRole.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'JobRole is missing' });
+        } else if (!department || department.trim() === '') {
+            res.status(400).json({ status: 'error', error: 'Department is missing' });
         } else {
-            db.query('SELECT id FROM roles WHERE LOWER(name) = $1', [jobRole.toLowerCase()]).then((roleDb) => {
-                if (roleDb.rowCount === 0) {
-                    res.status(404).json({ status: 'error', error: 'Role does not exist' });
-                } else {
-                    db.query('SELECT id FROM departments WHERE LOWER(name) = $1', [department.toLowerCase()]).then((departmentDb) => {
-                        if (departmentDb.rowCount === 0) {
-                            res.status(404).json({ status: 'error', error: 'Department does not exist' });
-                        } else {
-                            bcrypt.hash(password, 10).then((hashedPassword) => {
-                                const roleId = roleDb.rows[0].id;
-                                const departmentId = departmentDb.rows[0].id;
-                                const input = [
-                                    firstName, lastName, email, hashedPassword, gender,
-                                    address, roleId, departmentId,
-                                ];
+            try {
+                const resultR = await db.query('SELECT id FROM roles WHERE LOWER(name) = $1', [jobRole.toLowerCase()]);
 
-                                db.query('INSERT INTO users (first_name, last_name, email, password, gender, address, role_id, department_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id', input).then((result) => {
-                                    try {
-                                        const { id: userId } = result.rows[0];
-                                        const token = generateToken({ userId });
-
-                                        res.status(201).json({ status: 'success', data: { message: 'User account successfully created', token, userId } });
-                                    } catch (e2) {
-                                        console.error(e2.message || e2.error.message);
-                                        res.status(500).json({ status: 'error', error: 'The new user-account token could not be generated' });
-                                    }
-                                }).catch(() => {
-                                    res.status(400).json({ status: 'error', error: 'The user information could not be saved' });
-                                });
-                            }).catch(() => {
-                                res.status(500).json({ status: 'error', error: 'The password could not be encrypted' });
-                            });
-                        }
-                    }).catch(() => {
-                        res.status(500).json({ status: 'error', error: 'Department details cannot be retrieved' });
-                    });
+                if (resultR.rowCount === 0) {
+                    return res.status(404).json({ status: 'error', error: 'Role does not exist' });
                 }
-            }).catch(() => {
+
+                try {
+                    const resultD = await db.query('SELECT id FROM departments WHERE LOWER(name) = $1', [department.toLowerCase()]);
+
+                    if (resultD.rowCount === 0) {
+                        return res.status(404).json({ status: 'error', error: 'Department does not exist' });
+                    }
+
+                    try {
+                        const roleId = resultR.rows[0].id;
+                        const departmentId = resultD.rows[0].id;
+
+                        const hashedPassword = await bcrypt.hash(password, 10);
+                        const input = [
+                            firstName, lastName, email, hashedPassword, gender,
+                            address, roleId, departmentId,
+                        ];
+
+                        try {
+                            const result = await db.query('INSERT INTO users (first_name, last_name, email, password, gender, address, role_id, department_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING id', input);
+                            
+                            try {
+                                const { id: userId } = result.rows[0];
+                                const token = generateToken({ userId });
+
+                                res.status(201).json({ status: 'success', data: { message: 'User account successfully created', token, userId } });
+                            } catch (e5) {
+                                console.error(e5.message || e5.error.message);
+                                res.status(500).json({ status: 'error', error: 'The new user-account token could not be generated' });
+                            }
+                        } catch (e4) {
+                            console.error(e4.message || e4.error.message);
+                            res.status(500).json({ status: 'error', error: 'The user information could not be saved' });
+                        }
+                    } catch (e3) {
+                        console.error(e3.message || e3.error.message);
+                        res.status(500).json({ status: 'error', error: 'The password could not be encrypted' });
+                    }
+                } catch (e2) {
+                    console.error(e2.message || e2.error.message);
+                    res.status(500).json({ status: 'error', error: 'Department details cannot be retrieved' });
+                }
+            } catch (e) {
                 res.status(500).json({ status: 'error', error: 'Role details cannot be retrieved' });
-            });
+            }
         }
     },
 
     signIn: async (req, res) => {
         const { email, password } = req.body;
 
-        if (email === undefined || email.trim() === '') {
+        if (!email || email.trim() === '') {
             res.status(400).json({ status: 'error', error: 'E-mail address cannot be blank' });
-        } else if (password === undefined || password.trim() === '') {
+        } else if (!password || password.trim() === '') {
             res.status(400).json({ status: 'error', error: 'Password cannot be blank' });
         } else {
-            await db.query('SELECT id, first_name, password FROM users WHERE email = $1', [email], (error, result) => {
-                if (error) {
-                    res.status(500).json({ status: 'error', error: 'E-mail address verification failed' });
-                } else if (result.rowCount === 0) {
-                    res.status(404).json({ status: 'error', error: 'E-mail address cannot be found' });
-                } else {
-                    bcrypt.compare(password, result.rows[0].password, (err, same) => {
-                        if (err) {
-                            console.error(err.message || err.error.message);
-                            res.status(500).json({ status: 'error', error: 'Password verification failed' });
-                        } else if (same) {
-                            try {
-                                const { id: userId, first_name: firstName } = result.rows[0];
-                                const token = generateToken({ userId });
+            try {
+                const result = await db.query('SELECT id, first_name, password FROM users WHERE email = $1', [email]);
 
-                                res.status(200).json({ status: 'success', data: { token, userId, firstName } });
-                            } catch (e2) {
-                                console.error('', e2.message || e2.error.message);
-                                res.status(500).json({ status: 'error', error: 'Personal information could not be retrieved' });
-                            }
-                        } else {
-                            res.status(401).json({ status: 'error', error: 'Invalid email and/or password' });
-                        }
-                    });
+                if (result.rowCount === 0) {
+                    return res.status(404).json({ status: 'error', error: 'E-mail address cannot be found' });
                 }
-            });
+
+                try {
+                    const same = await bcrypt.compare(password, result.rows[0].password);
+
+                    if (!same) {
+                        return res.status(401).json({ status: 'error', error: 'Invalid email and/or password' });
+                    }
+
+                    const { id: userId, first_name: firstName } = result.rows[0];
+                    const token = generateToken({ userId });
+
+                    res.status(200).json({ status: 'success', data: { token, userId, firstName } });
+                } catch (e2) {
+                    console.error('', e2.message || e2.error.message);
+                    res.status(500).json({ status: 'error', error: 'Password verification failed' });
+                }
+            } catch (error) {
+                res.status(500).json({ status: 'error', error: 'E-mail address verification failed' });
+            }
         }
     },
 };
