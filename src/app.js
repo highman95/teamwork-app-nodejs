52125955eef8efee9712123cc88e0826ea818f92
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-01-15 12:39:49 +0100

    refactor(server): add static-routes and error middleware

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
@@ -19,7 +20,11 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/v1', routes(router));
+app.use(express.static(path.join(__dirname, '../public')));
+app.use('/api/v1', routes(router), (err, req, res, next) => {
+    const isCSE = (err.name === 'TokenExpiredError') || (err.name === 'TypeError') || (err.name === 'Error');
+    res.status(isCSE ? 400 : 500).send({ status: 'error', error: err.message || err.error.message });
+});
 
 // set a default route
 app.use('*', (req, res) => res.status(404).json({ status: 'error', error: 'Page no longer exists' }));
--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 
-const http = require('http');
 const app = require('./app');
 
-let server = http.createServer(app);
-server = server.listen(process.env.PORT || 3000, (error) => {
+const server = app.listen(process.env.PORT || 3000, (error) => {
     console.log(error ? `Error: ${error}...` : `Listening on PORT: ${server.address().port}`);
 });
 
