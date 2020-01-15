1e368cd5cbfcef56ac86320c765d3cee565f3783
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-01-15 13:03:21 +0100

    chore(feed): add pagination

--- src/controllers/feed.js
+++ src/controllers/feed.js
@@ -2,8 +2,15 @@ const db = require('../configs/db');
 
 module.exports = {
     getPosts: async (req, res) => {
+        const { page = 1, chunk: limit = 10 } = req.query;
+
         try {
-            const result = await db.query('SELECT p.id, title, content, image_url, user_id, post_type_id, created_at, name FROM posts p JOIN post_types pt ON pt.id = p.post_type_id ORDER BY created_at DESC');
+            const resultCount = await db.query(`SELECT COUNT(p.id) as total_count FROM posts p JOIN post_types pt ON pt.id = p.post_type_id`);
+            const totalCount = parseInt(resultCount.rows[0].total_count);
+
+            const currentPage = parseInt(page);
+            const offset = ((currentPage < 1 ? 1 : currentPage) - 1) * limit;
+            const result = await db.query(`SELECT p.id, title, content, image_url, user_id, post_type_id, created_at, name FROM posts p JOIN post_types pt ON pt.id = p.post_type_id ORDER BY created_at DESC LIMIT ${limit} OFFSET ${offset}`);
 
             const feeds = result.rows.map((row) => {
                 const {
@@ -21,7 +28,9 @@ module.exports = {
                 };
             });
 
-            res.status(200).json({ status: 'success', data: feeds });
+            const pages = limit < 1 ? 1 : Math.ceil(totalCount / limit)
+            const meta = { currentPage, pages, totalCount }
+            res.status(200).json({ status: 'success', data: feeds, meta });
         } catch (e) {
             console.error('[Feed] DB-Error', e.message || e.error.message);
             res.status(500).json({ status: 'error', error: 'The article/gif posts could not be retrieved' });
