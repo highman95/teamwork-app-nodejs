ebad90717647b9128f1c6576aff407bd7b8e0bba
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-03-29 00:04:17 +0100

    refactor(route): renamed controller reference

--- src/routes/article.js
+++ src/routes/article.js
@@ -1,10 +1,10 @@
-const articleController = require('../controllers/article');
+const controller = require('../controllers/article');
 const auth = require('../middlewares/auth');
 
 module.exports = (router) => {
-    router.post('/articles', auth, articleController.createPost);
-    router.post('/articles/:articleId/comment', auth, articleController.createPostComment);
-    router.patch('/articles/:articleId', auth, articleController.updatePost);
-    router.get('/articles/:articleId', auth, articleController.getPost);
-    router.delete('/articles/:articleId', auth, articleController.deletePost);
+    router.post('/articles', auth, controller.createPost);
+    router.post('/articles/:articleId/comment', auth, controller.createPostComment);
+    router.patch('/articles/:articleId', auth, controller.updatePost);
+    router.get('/articles/:articleId', auth, controller.getPost);
+    router.delete('/articles/:articleId', auth, controller.deletePost);
 };
