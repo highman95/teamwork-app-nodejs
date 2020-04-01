890e0245140f66668d683b2855a7968c683fc66f
Emmanuel NWAMAIFE <emmanuel.nwamaife@interswitchgroup.com>
2020-04-01 05:19:34 +0100

    chore(test): add duration to test-report

--- spec/helpers/reporter.js
+++ spec/helpers/reporter.js
@@ -2,5 +2,5 @@ const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 
 jasmine.getEnv().clearReporters();             // remove default reporter logs
 jasmine.getEnv().addReporter(new SpecReporter({// add jasmine-spec-reporter
-    spec: { displayPending: true }
+    spec: { displayPending: true, displayDuration: true }
 }));
