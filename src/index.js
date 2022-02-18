5a4a31af0fe7a222296b21a6a45a069d8e1170a9
Emma NWAMAIFE <emadimabua@gmail.com>
2022-02-18 14:34:20 +0100

    chore(audit): fix npm-audit issue + minor bug

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ require('dotenv').config();
 const app = require('./app');
 
 // app.set('port', process.env.PORT || 3000);
-const server = app.listen(process.env.PORT || 3000, (error) => {
-  console.log(error ? `Error: ${error.message}...` : `Listening on PORT: ${server.address().port}`);
-}).on('error', (error) => {
-  console.error('Error-event occurred: ', error.message);
-});
+const server = app
+  .listen(process.env.PORT || 3000, () => {
+    console.log(`Listening on PORT: ${server.address().port}`);
+  })
+  .on('error', (error) => {
+    console.error('Error-event occurred: ', error.message);
+  });
 
 module.exports = server;
