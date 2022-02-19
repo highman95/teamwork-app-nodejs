c23c33aba1ed7afad5395f664b0f9b55a38c29e4
Emma NWAMAIFE <emadimabua@gmail.com>
2022-02-19 22:51:28 +0100

    test: added specs for user-service

--- spec/unit-tests/user.spec.js
+++ spec/unit-tests/user.spec.js
@@ -2,9 +2,26 @@
 const userService = require('../../src/models/user');
 
 describe('User-Service Test Suite', () => {
+  const account = {
+    firstName: 'Mark',
+    lastName: 'Spencer',
+    email: `mark.spencer-${Date.now()}@oc.com`,
+    password: 'markspencer',
+    gender: 'male',
+    address: '',
+    jobRole: 'staff',
+    department: 'Engineering',
+  };
+
   beforeAll(() => {});
 
   describe('Create User', () => {
+    let email = '';
+
+    beforeEach(() => {
+      email = `mark.spencer-${Date.now()}@oc.com`;
+    });
+
     describe('e-mail is not specified', () => {
       let responseBox = {};
 
@@ -18,6 +35,111 @@ describe('User-Service Test Suite', () => {
       });
 
       it('should return error message', () => expect(responseBox.message).toBe('E-mail address is invalid'));
+    });
+
+    describe('User-details are valid/correct', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, account.lastName, account.email, account.password, account.gender, account.address, account.jobRole, account.department)
+          .then((result) => {
+            responseBox = result;
+            done();
+          });
+      });
+
+      it('should return the user-id', () => expect(responseBox.id).toBeDefined());
+    });
+
+    describe('First-name is not specified', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(null, account.lastName, email, null, null, null, null, null)
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('FirstName cannot be empty (required)'));
+    });
+
+    describe('Last-name is not specified', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, null, email, null, null, null, null, null)
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('LastName cannot be empty (required)'));
+    });
+
+    describe('Password is not specified', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, account.lastName, email, '', account.gender, account.address, account.jobRole, account.department)
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('Password cannot be empty (required)'));
+    });
+
+    describe('Gender is not specified', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, account.lastName, email, account.password, null, account.address, null, null)
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('Gender cannot be empty (required)'));
+    });
+
+    describe('Role is not valid', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, account.lastName, email, account.password, account.gender, account.address, 'enjoyment-officer', null)
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('Role does not exist'));
+    });
+
+    describe('Department is not valid', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService
+          .create(account.firstName, account.lastName, email, account.password, account.gender, account.address, account.jobRole, 'enjoyment-dept')
+          .catch((error) => {
+            responseBox = error;
+            done();
+          });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('Department does not exist'));
     });
   });
 
@@ -46,6 +168,80 @@ describe('User-Service Test Suite', () => {
       });
 
       it('should return error message', () => expect(responseBox.message).toBe('Invalid e-mail / password'));
+    });
+
+    describe('Password is incorrect', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService.authenticate(account.email, `${account.password}12`).catch((error) => {
+          responseBox = error;
+          done();
+        });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('Invalid e-mail / password'));
+    });
+
+    describe('Valid credentials are specified', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService.authenticate(account.email, account.password).then((result) => {
+          responseBox = result;
+          done();
+        });
+      });
+
+      it('should return the same e-mail', () => expect(responseBox.email).toBe(account.email));
+      it('should return the user\'s first-name', () => expect(responseBox.first_name).toBe(account.firstName));
+      it('should return the user\'s last-name', () => expect(responseBox.last_name).toBe(account.lastName));
+      it('should return the user-id', () => expect(responseBox.id).toBeDefined());
+      it('should return the user\'s password', () => expect(responseBox.password).toBeDefined());
+    });
+  });
+
+  describe('Find User By E-mail', () => {
+    describe('E-mail is not valid', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService.findByEmail('geek@').catch((error) => {
+          responseBox = error;
+          done();
+        });
+      });
+
+      it('should return error message', () => expect(responseBox.message).toBe('E-mail address is invalid'));
+    });
+
+    describe('E-mail is not registered', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService.findByEmail(`geek-${Date.now()}@getnada.com`).then((result) => {
+          responseBox = result;
+          done();
+        });
+      });
+
+      it('should return empty-object', () => expect(responseBox).toEqual({}));
+    });
+
+    describe('E-mail is valid/registered', () => {
+      let responseBox = {};
+
+      beforeAll((done) => {
+        userService.findByEmail(account.email).then((result) => {
+          responseBox = result;
+          done();
+        });
+      });
+
+      it('should return same e-mail', () => expect(responseBox.email).toEqual(account.email));
+      it('should return the first-name', () => expect(responseBox.first_name).toEqual(account.firstName));
+      it('should return the last-name', () => expect(responseBox.last_name).toEqual(account.lastName));
+      it('should return the user-id', () => expect(responseBox.id).toBeDefined());
     });
   });
 });
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -3,7 +3,7 @@ module.exports.isValidEmail = (email) => (email && /^([a-zA-Z0-9_\-]+)(\.)?([a-z
 module.exports.validateParameters = (submissions, params) => {
   Object.entries(submissions).map(([key, value]) => {
     if (!params.includes(key)) throw new Error(`Invalid Parameter - ${key} - supplied`); // 400
-    if (!value.trim()) throw new Error(`${key.charAt(0).toUpperCase().concat(key.substring(1))} cannot be empty (required)`); // 400
+    if (!value || !value.trim()) throw new Error(`${key.charAt(0).toUpperCase().concat(key.substring(1))} cannot be empty (required)`); // 400
     return { [key]: value };
   });
 };
